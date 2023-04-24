caa8fd50d4c91e06271b57635fa80874390fe822
saqlainpathan9 <96249640+saqlainpathan9@users.noreply.github.com>
2023-04-24 19:01:38 +0530

    Add files via upload

---
+++ angular8-springboot-client/src/app/employee.ts
@@ -0,0 +1,11 @@
+export class Employee {
+  id: number;
+  firstName: string;
+  lastName: string;
+  emailId: string;
+  address: string;
+  contact: string;
+  department: string;
+  designation: string;
+  active: boolean;
+}
